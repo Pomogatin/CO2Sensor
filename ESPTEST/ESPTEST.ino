c3b39bdfaffab453ae01f44df7ff7e6d2934168f
Pomogatin <38038463+Pomogatin@users.noreply.github.com>
2018-04-03 17:12:13 +0300

    Add files via upload

---
+++ ESPTEST/ESPTEST.ino
@@ -0,0 +1,27 @@
+#include <ESP8266WiFi.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+#include <SoftwareSerial.h>
+SoftwareSerial WiFiSerial(0, 1);
+void setup()
+{
+  Serial.begin(115200);
+  WiFiSerial.begin(115200);
+  Serial.println();
+
+ WiFi.begin("имя", "пароль");
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {}
